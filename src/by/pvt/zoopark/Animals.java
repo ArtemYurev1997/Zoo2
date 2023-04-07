0a0d38b50c59b41fdd958556facccf4b8cf8dd1e
artem <artem.yurev.1997@gmail.com>
2023-04-07 18:49:07 +0300

    task

--- src/by/pvt/zoopark/Animals.java
+++ src/by/pvt/zoopark/Animals.java
@@ -1,59 +1,5 @@
 package by.pvt.zoopark;
 
 public abstract class Animals {
-    private String color;
-    private int maxLife;
-    private String typeOfEat;
-    static int nextId = 1;
-    private int id;
 
-    public Animals(String color, int maxLife, String typeOfEat) {
-        id = 0;
-        this.color = color;
-        this.maxLife = maxLife;
-        this.typeOfEat = typeOfEat;
-    }
-
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId() {
-        id = nextId;
-        nextId++;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setMaxLife(int maxLife) {
-        this.maxLife = maxLife;
-    }
-
-    public void setTypeOfEat(String typeOfEat) {
-        this.typeOfEat = typeOfEat;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public int getMaxLife() {
-        return maxLife;
-    }
-
-    public String getTypeOfEat() {
-        return typeOfEat;
-    }
-
-    public abstract void sound();
-    public void play() {
-        System.out.println("Животное играет");
-    }
-    public String toString() {
-        return "Цвет: " + color + " " + "Максимальное количество жизни: " + maxLife + " " + "Тип еды: " + typeOfEat;
-    }
 }
